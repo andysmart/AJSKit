d3e8ced907a65fc5c19521cff5dd7935c3a7b0a5
Andy Smart <andy@andysmart.org>
2011-09-10 10:45:21 +0100

    Initial Commit

---
+++ AJSKit/AJSKit.h
@@ -0,0 +1,13 @@
+//
+//  AJSKit.h
+//  AJSKit
+//
+//  Created by Andy Smart on 10/09/2011.
+//  Copyright 2011 Andy Smart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AJSKit : NSObject
+
+@end
---
+++ AJSKitTests/AJSKitTests.h
@@ -0,0 +1,13 @@
+//
+//  AJSKitTests.h
+//  AJSKitTests
+//
+//  Created by Andy Smart on 10/09/2011.
+//  Copyright 2011 Andy Smart. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AJSKitTests : SenTestCase
+
+@end
